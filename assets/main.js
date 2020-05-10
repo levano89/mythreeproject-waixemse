75e248b4289d818436c09c85ba6dd2a03bae8b36
levano89 <61513108+levano89@users.noreply.github.com>
2020-05-10 12:41:31 +0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -6,14 +6,6 @@ document.getElementsByClassName('burger')[0].addEventListener('click', function(
     document.getElementsByClassName("line")[2].classList.toggle("line-3-active")
 })
 
-// function burger( ){ 
-//     let  menu = document.getElementById("search-2")
-//     menu.classList.toggle("search-2-active")
-//     document.getElementsByClassName("line")[0].classList.toggle("line-1-active")
-//     document.getElementsByClassName("line")[1].classList.toggle("line-2-active")
-//     document.getElementsByClassName("line")[2].classList.toggle("line-3-active")
-// }
-
 let addbuttons = document.getElementsByClassName('addFood');
 let bl = addbuttons.length;
 console.log(bl);
